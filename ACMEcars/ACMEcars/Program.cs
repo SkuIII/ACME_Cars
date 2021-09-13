87de090058c861d93819b397e94c3ab48e5c546f
Oscar <oscargus0247@gmail.com>
2021-09-13 12:25:50 +0200

    Jobbar med grunderna

--- ACMEcars/ACMEcars/Program.cs
+++ ACMEcars/ACMEcars/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ACMEcars
 {
@@ -6,7 +7,48 @@ namespace ACMEcars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Car> myCars = new List<Car>(); 
+
+            while (true)
+            {
+                Console.WriteLine("Välj vad du vill göra med hjälp av alternativen nedan:\n" +
+                                  "1 (Skapa ny kund)\n" +
+                                  "2 (Visa antal kunder)\n" +
+                                  "3 (Visa lista över alla kunder)\n" +
+                                  "4 (Avsluta)");
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "N":
+                        Console.WriteLine("Add car");
+                        break;
+
+                    case "E":
+                        Console.WriteLine("Edit existing car");
+                        break;
+
+                    case "D":
+                        Console.WriteLine("Remove car");
+                        break;
+
+                    case "S":
+                        Console.WriteLine("Show all cars");
+                        break;
+
+                    default:
+                        Console.WriteLine("TEST");
+                        break;
+                }
+            }
         }
+    }
+    class Car
+    {
+        public string Color;
+        public string Price;
+        public string Model;
+        public string Make;
+        public string Year;
     }
 }
